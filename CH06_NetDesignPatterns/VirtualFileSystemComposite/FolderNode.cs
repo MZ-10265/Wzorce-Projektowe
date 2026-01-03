3e46199112950b650f0d6eff7474e95ab75e6c1d
MZ-10265 <s10265mz@ms.wwsi.edu.pl>
2026-01-03 10:30:33 +0100

    Update

---
+++ CH06_NetDesignPatterns/VirtualFileSystemComposite/FolderNode.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+//Folder może zawierać inne element: pliki, foldery
+//folder implementuje ten sam interfejs INode co FileNode ale ma listę dzieci i działa rekurencyjnie
+
+public sealed class FolderNode : INode
+{
+    public string Name { get; }
+
+    // Dzieci folderu: mogą być FileNode lub FolderNode, ale trzymamy je jako INode, żeby klient nie musiał znać typów
+    private readonly List<INode> _children = new();
+
+    public FolderNode(string name)
+    {
+        Name = name;
+    }
+
+    //dodawanie elementu do folderu.
+    public void Add(INode node) => _children.Add(node);
+
+    // Usuwanie elementu z folderu.
+    public void Remove(INode node) => _children.Remove(node);
+
+    public int GetSize()
+    {
+        //Rekurencja: rozmiar folderu = suma rozmiarów dzieci
+        int sum = 0;
+        foreach (var child in _children)
+        {
+            sum += child.GetSize();
+        }
+        return sum;
+    }
+
+    public void Print(string indent = "")
+    {
+        //najpierw wypisujemy folder
+        Console.WriteLine($"{indent}+ {Name} ({GetSize()} KB)");
+        //potem wszystkie dzieci w środku(rekurencja)
+        foreach (var child in _children)
+        {
+            child.Print(indent + "  ");
+        }
+    }
+}
